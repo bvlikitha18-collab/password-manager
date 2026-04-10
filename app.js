73e2058f9dfd41b14b500ce016361f034b199670
Likitha <bvlikitha18@gmail.com>
2026-04-10 14:48:00 +0530

    v1 basic password manager

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.use(express.json());
+
+const FILE = 'data.json';
+
+// Read data
+function readData() {
+    return JSON.parse(fs.readFileSync(FILE));
+}
+
+// Write data
+function writeData(data) {
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+}
+
+// Add credentials
+app.post('/add', (req, res) => {
+    const data = readData();
+    data.push(req.body);
+    writeData(data);
+    res.send("Added");
+});
+
+// View credentials
+app.get('/view', (req, res) => {
+    res.json(readData());
+});
+
+// Delete credentials
+app.delete('/delete/:id', (req, res) => {
+    let data = readData();
+    data = data.filter((_, i) => i != req.params.id);
+    writeData(data);
+    res.send("Deleted");
+});
+
+app.listen(3000, () => console.log("Running on port 3000"));
